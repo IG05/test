8edb184b1ef33025b7bae825de4b49355e6deada
IG05 <113109099+IG05@users.noreply.github.com>
2025-05-26 11:51:07 +0530

    Update app.test.js

--- app-nodejs/__tests__/app.test.js
+++ app-nodejs/__tests__/app.test.js
@@ -1,3 +1,10 @@
-test('Basic test - always passes', () => {
-  expect(1 + 1).toBe(2);
+const request = require('supertest');
+const app = require('../server'); // <- import the Express app
+
+describe('Basic test', () => {
+  it('should return 200 OK and expected message', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe('Hello from Node.js App (Blue-Green Deployment)');
+  });
 });
