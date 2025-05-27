168dad214b1d36c0d6f479438b2eee9982920408
IG05 <ishaan2004goyal@gmail.com>
2025-05-27 10:26:15 +0530

    don

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
   res.send('Hello from Node.js App (Blue-Green Deployment)');
 });
 
+// 404 handler for unknown routes
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
 if (require.main === module) {
   app.listen(port, () => {
     console.log(`Node.js app listening at http://localhost:${port}`);
