eef045903d3f81f81375ea0b73a038c0d84b62d7
IG05 <113109099+IG05@users.noreply.github.com>
2025-06-17 11:22:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.disable('x-powered-by');
 
+app.get('/health', (req, res) => {
+  res.status(500).send('Health check failed');
+});
+
 app.get('/', (req, res) => {
   res.send('Bye from Node.js App (Blue-Green Deployment)');
 });
