18f1e62c67fea5995a41865ec222372c87e5674a
IG05 <ishaan2004goyal@gmail.com>
2025-06-25 11:12:15 +0530

    first

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["lcov", "text","html"],
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "reports/junit",
+        outputName: "junit.xml"
+      }
+    ],
+    [
+      "@casualbot/jest-sonar-reporter",
+      {
+        outputDirectory: "coverage",
+        outputName: "sonar-report.xml",
+        relativePaths: true
+      }
+    ]
+  ],
+  roots: ["<rootDir>/"],            // adjust if your source code is inside a folder like 'src'
+  collectCoverageFrom: [
+    "**/*.js",
+    "!**/node_modules/**",
+    "!**/__tests__/**",
+    "!**/jest.config.js"
+    
+  ]
+};
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.disable('x-powered-by');
+
+app.get('/health', (req, res) => {
+  res.send('Health check passed intentionally');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+module.exports = app;
